b14f38a326d67220e910ddcf3f9f55bd06cf9654
hema-manohar <121711987+hema-manohar@users.noreply.github.com>
2023-01-03 20:50:22 +0530

    #include<stdio.h> int main() {     int p,i;     scanf("%d",&p);     for(i=1;i<=p;i++)     {          int m,a;     scanf("%d%d",&m,&a);     printf("%d ",m+a);     }
    }

---
+++ sum_of_the_given_numbers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int p,i;
+    scanf("%d",&p);
+    for(i=1;i<=p;i++)
+    {
+         int m,a;
+    scanf("%d%d",&m,&a);
+    printf("%d
+",m+a);
+    }
+   
+}
