2819376544284a29f349ea2dd9aed677538d6408
hema-manohar <121711987+hema-manohar@users.noreply.github.com>
2023-02-05 20:37:37 +0530

    #include<stdio.h> int main() {     int x,y,z,m;     scanf("%d%d%d",&x,&y,&z);     m=(z-y)/x;     printf("%d",m); }

--- Can_cross_the_bridge.c
+++ Can_cross_the_bridge.c
@@ -1,13 +1,8 @@
 #include<stdio.h>
-void can(int x,int y,int z)
-{
-    int n;
-    n=(z-y)/x;
-    printf("%d",n);
-}
 int main()
 {
-    int x,y,z;
+    int x,y,z,m;
     scanf("%d%d%d",&x,&y,&z);
-    can (x,y,z);
+    m=(z-y)/x;
+    printf("%d",m);
 }
